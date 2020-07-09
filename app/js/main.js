908f8aa31000b9329eab3078b5262e06291b1ea2
AndrewS <andrews@AndrewS.lan1>
2020-07-09 08:56:22 -1000

    Add new icons.

---
+++ app/js/main.js
@@ -0,0 +1,97 @@
+const { ipcRenderer } = require('electron');
+
+//Fields.
+const file = document.getElementById('file');
+const lastName = document.getElementById('last_name');
+const firstName = document.getElementById('first_name');
+const autocompleteTreatments = document.getElementById('treatments');
+const autocompleteBody = document.getElementById('body');
+const treatmentDate = document.getElementById('treatment_date');
+const providerInitals = document.getElementById('provider_initals');
+
+// Treatment Date Picker.
+const today = new Date();
+treatmentDate.value =
+	today.getFullYear().toString() +
+	'-' +
+	(today.getMonth() + 1).toString().padStart(2, 0) +
+	'-' +
+	today.getDate().toString().padStart(2, 0);
+
+// Selects.
+var selectElems = document.querySelectorAll('select');
+var selectInstances = M.FormSelect.init(selectElems);
+
+// Autocomplete Treatments.
+const autocompleteTreatmentOptions = {
+	data: {
+		Coolsculpting: null,
+		Emsculpt: null,
+		T0X: null,
+		Filler: null,
+		AFT: null,
+		LHR: null
+	}
+};
+const autocompleteTreatmentInstances = M.Autocomplete.init(autocompleteTreatments, autocompleteTreatmentOptions);
+
+// Autocomplete Body.
+const autocompleteBodyOptions = {
+	data: {
+		Abdominal: null,
+		Anus: null,
+		Face: null,
+		Brazilian: null,
+		Underarm: null,
+		Back: null
+	}
+};
+const autocompleteBodyInstances = M.Autocomplete.init(autocompleteBody, autocompleteBodyOptions);
+
+// Form.
+document.getElementById('hms_form').addEventListener('submit', (e) => {
+	e.preventDefault();
+	console.log(document.getElementById('treatment_date').value);
+	const filePath = file.files[0].path;
+	const lastNameValue = lastName.value;
+	const firstNameValue = firstName.value;
+	const dobValue = document.getElementById('dob').value ? getDefaultDate(document.getElementById('dob').value) : '';
+	const autocompleteTreatmentsValue = autocompleteTreatments.value;
+	const autocompleteBodyValue = autocompleteBody.value;
+	const treatmentDateValue = document.getElementById('treatment_date').value
+		? getDefaultDate(document.getElementById('treatment_date').value)
+		: '';
+	const providerInitalsValue = providerInitals.value;
+	ipcRenderer.send('file:rename', {
+		filePath,
+		lastNameValue,
+		firstNameValue,
+		dobValue,
+		autocompleteTreatmentsValue,
+		autocompleteBodyValue,
+		treatmentDateValue,
+		providerInitalsValue
+	});
+});
+
+function getDefaultDate(curDate) {
+	var dt = new Date(curDate);
+	var date = (dt.getDate() + 1).toString().padStart(2, '0');
+	var month = (dt.getMonth() + 1).toString().padStart(2, '0');
+	var year = dt.getFullYear();
+	return month.toString() + date.toString() + year.toString();
+}
+
+// Rename done.
+ipcRenderer.on('rename:done', (newFileName) => {
+	document.getElementById('hms_form').reset();
+	treatmentDate.value =
+		today.getFullYear().toString() +
+		'-' +
+		(today.getMonth() + 1).toString().padStart(2, 0) +
+		'-' +
+		today.getDate().toString().padStart(2, 0);
+	M.toast({
+		html: `File renamed`
+	});
+});
--- main.js
+++ main.js
@@ -1,117 +1,138 @@
 const path = require('path');
-const os = require('os');
-const { app, BrowserWindow, Menu, ipcMain, shell } = require('electron');
+const fs = require('fs');
+const { app, BrowserWindow, Menu, ipcMain } = require('electron');
 const slash = require('slash');
 
-
 // Set enviroment.
-process.env.NODE_ENV = 'development';
+process.env.NODE_ENV = 'production';
 
-const isDev = process.env.NODE_ENV !== 'product' ? true : false;
+const isDev = process.env.NODE_ENV !== 'production' ? true : false;
 const isMac = process.platform === 'darwin' ? true : false;
 
 let mainWindow;
 let aboutWindow;
 
-function createMainWindow () {
-  mainWindow = new BrowserWindow({
-    title: 'ImageShrink',
-    width: isDev ? 800 : 500,
-    height: 600,
-    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
-    resizable: isDev ? true : false,
-    backgroundColor: 'white',
-    webPreferences: {
-      nodeIntegration: true
-    }
-  });
+function createMainWindow() {
+	mainWindow = new BrowserWindow({
+		title: 'HMS DOC Rename',
+		width: 600,
+		height: 750,
+		icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+		resizable: true,
+		backgroundColor: '#fff',
+		webPreferences: {
+			nodeIntegration: true
+		}
+	});
 
-  if(isDev) {
-    mainWindow.webContents.openDevTools();
-  }
+	if (isDev) {
+		mainWindow.webContents.openDevTools();
+	}
 
-  // Load index.html into window.
-  mainWindow.loadFile('./app/index.html');
+	// Load index.html into window.
+	mainWindow.loadFile('./app/index.html');
 }
 
 // About Window.
-function createAboutWindow () {
-  aboutWindow = new BrowserWindow({
-    title: 'About Image Shrink',
-    width: 300,
-    height: 300,
-    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
-    resizable: false,
-    backgroundColor: 'white'
-  });
+function createAboutWindow() {
+	aboutWindow = new BrowserWindow({
+		title: 'HMS Doc Formatter',
+		width: 300,
+		height: 300,
+		icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+		resizable: false,
+		backgroundColor: 'white'
+	});
 
-  // Load index.html into window.
-  aboutWindow.loadFile('./app/about.html');
+	// Load index.html into window.
+	aboutWindow.loadFile('./app/about.html');
 }
 
 app.on('ready', () => {
-  createMainWindow();
+	createMainWindow();
 
-  const mainMenu = Menu.buildFromTemplate(menu);
-  Menu.setApplicationMenu(mainMenu);
+	const mainMenu = Menu.buildFromTemplate(menu);
+	Menu.setApplicationMenu(mainMenu);
 
-
-  mainWindow.on('closed', () => mainWindow = null);
+	mainWindow.on('closed', () => (mainWindow = null));
 });
 
 const menu = [
-  ...(isMac ? [
-    {
-      label: app.name,
-      submenu: [
-        {
-          label: 'About',
-          click: createAboutWindow
-        }
-      ]
-    }
-  ]: []),
-  {
-    role: 'fileMenu'
-  },
-  ...(!isMac ? [
-    {
-      lable: 'Help',
-      submenu: [
-        {
-          label: 'About',
-          click: createAboutWindow
-        }
-      ]
-    }
-  ] : []),
-  ...(isDev ? [{
-    label: 'developer',
-    submenu: [
-      {role: 'reload'},
-      {role: 'forcereload'},
-      {type: 'separator'},
-      {role: 'toggledevtools'}
-    ]
-  }]: [])
-]
+	...(isMac
+		? [
+				{
+					label: app.name,
+					submenu: [
+						{
+							label: 'About',
+							click: createAboutWindow
+						}
+					]
+				}
+			]
+		: []),
+	{
+		role: 'fileMenu'
+	},
+	...(!isMac
+		? [
+				{
+					lable: 'Help',
+					submenu: [
+						{
+							label: 'About',
+							click: createAboutWindow
+						}
+					]
+				}
+			]
+		: []),
+	...(isDev
+		? [
+				{
+					label: 'developer',
+					submenu: [
+						{ role: 'reload' },
+						{ role: 'forcereload' },
+						{ type: 'separator' },
+						{ role: 'toggledevtools' }
+					]
+				}
+			]
+		: [])
+];
 
 // Catch form submit
-ipcMain.on('image:minimizer', (e, options) => {
-  options.dest = path.join(os.homedir(), 'imageshrink')
-  console.log('sup');
-})
+ipcMain.on('file:rename', (e, options) => {
+	const oldFilepath = slash(options.filePath);
+	const fileExt = path.extname(oldFilepath);
+	const fileDir = path.dirname(oldFilepath);
 
-
+	// TODO: Need to double check DOB placement.
+	const newFileName = `${options.lastNameValue},${options.firstNameValue}${options.dobValue
+		? `_${options.dobValue}`
+		: ''}_${options.autocompleteTreatmentsValue}
+  ${options.autocompleteBodyValue
+		? `_${options.autocompleteBodyValue}_`
+		: '_'}${options.treatmentDateValue}${options.providerInitalsValue ? `_${options.providerInitalsValue}` : ''}`;
+	const newFilePath = slash(`${fileDir + `/${newFileName.replace(/\s/g, '')}` + fileExt}`);
+	fs.rename(oldFilepath, newFilePath, (error) => {
+		if (error) {
+			console.log(error);
+		} else {
+			mainWindow.webContents.send('rename:done');
+		}
+	});
+});
 
 app.on('window-all-closed', () => {
-  if (!isMac) {
-    app.quit()
-  }
-})
+	if (!isMac) {
+		app.quit();
+	}
+});
 
 app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createMainWindow()
-  }
-})
+	if (BrowserWindow.getAllWindows().length === 0) {
+		createMainWindow();
+	}
+});
